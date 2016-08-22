2aeb2b90264e097b551e32a7193042e2c52406b6
tens04 <tens04@chen.local>
2016-08-22 15:07:34 +0800

    新的项目

---
+++ Tens_demo/Tens_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tens_demo
+//
+//  Created by tens04 on 16/8/22.
+//  Copyright © 2016年 tens十安科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
